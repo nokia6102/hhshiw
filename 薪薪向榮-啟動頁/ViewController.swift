a22a838a7e880f8551537ec196db9b5a05bbb631
Leo Chen <unision@gmail.com>
2017-07-08 16:44:21 +0800

    Initial Commit

---
+++ 薪薪向榮-啟動頁/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  薪薪向榮-啟動頁
+//
+//  Created by leo_unision on 2017/7/8.
+//  Copyright © 2017年 unision. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
